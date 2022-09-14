7241883ef6c5594f08157e9823c3102f4ac22ac3
shannu-3 <80533721+shannu-3@users.noreply.github.com>
2022-09-14 21:35:43 +0530

    update

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -23,15 +23,6 @@ const About = () => {
         <li>B Mohitha</li>
         <li>Shanawaz Baig</li>
       </ul>
-      <h1>Attributions:</h1>
-      <ul>
-        <a href="http://products.wolframalpha.com/api/"><li>Wolfram Alpha Api</li></a>
-        <a href="https://codepen.io/mrrocks/pen/EiplA"><li>Loading Spinner</li></a>
-        <a href="https://codepen.io/seansean11/pen/dhwzj"><li>Pulse Animation</li></a>
-        <a href="https://sweetalert.js.org/"><li>SweetAlert</li></a>
-        <a href="https://gist.github.com/hsed/ef4a2d17f76983588cb6d2a11d4566d6"><li>Speech Synthesis Chunker</li></a>
-        <li>Icons: <a href="https://www.flaticon.com/free-icon/microphone-black-shape_25682#term=mic&page=1&position=1">Mic</a> | <a href="https://www.flaticon.com/free-icon/left-arrow_61752#term=back%20arrow&page=1&position=17">Arrow</a></li>
-      </ul>
     </div>
   );
 }
